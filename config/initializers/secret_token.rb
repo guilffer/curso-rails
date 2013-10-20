d1917c602d28404abba921584f62261bdce4855a
Renata <renobre@renobre-note.(none)>
2013-10-20 09:40:50 -0200

    Configurações iniciais, crud users, crud posts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogUnifal::Application.config.secret_key_base = '059210e53e0a40ce94d1b68ed8f2d4dd3f335ef40b0ac6ab54251b3910c866114f59779bc096be909304ae3a92f58c3209159cff0ae3f7d6a412755cbef0cf98'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BlogUnifal::Application.config.session_store :cookie_store, key: '_blog-unifal_session'
